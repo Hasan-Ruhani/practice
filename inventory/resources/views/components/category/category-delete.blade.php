7624f35a633855295c06d3c0d820d646232a2db4
H A S A N <hasan25042019@gmail.com>
2023-07-30 22:11:06 +0600

    Update category-delete.blade.php
    dff

--- inventory/resources/views/components/category/category-delete.blade.php
+++ inventory/resources/views/components/category/category-delete.blade.php
@@ -6,13 +6,14 @@
             <div class="modal-body text-center">
                 <h3 class=" mt-3 text-warning">Delete !</h3>
                 <p class="mb-3">Once delete, you can't get it back.</p>
+                <input class="d-none" id="deleteID">
             </div>
 
             <!-- Modal footer -->
             <div class="modal-footer justify-content-end">
                 <div>
-                    <button type="button"  class="btn shadow-sm btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" id="confirmDelete" class="btn shadow-sm btn-danger" >Delete</button>
+                    <button type="button" id="delete-modal-close"  class="btn shadow-sm btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" onclick="itemDelete()" id="confirmDelete" class="btn shadow-sm btn-danger" >Delete</button>
                 </div>
             </div>
 
@@ -21,3 +22,23 @@
 </div>
 
   
+<script>
+
+    async function itemDelete() {
+        let id = document.getElementById('deleteID').value;
+        document.getElementById('delete-modal-close').click();
+
+        showLoader();
+        let res = await axios.post("/delete-category", {id:id});
+        hideLoader();
+
+        if(res.data === 1){
+            successToast("Delete Complete");
+            await getList();
+        }
+        else{
+            errorToast("Somthing went wrong");
+        }
+    }
+
+</script>
