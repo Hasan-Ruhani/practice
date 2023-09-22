9752337271595e4d6970e4a142d2e8ce0faf2c58
H A S A N <hasan25042019@gmail.com>
2023-09-22 22:51:59 +0600

    fdg

---
+++ apple_p/app/Models/ProductDetaile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductDetaile extends Model
+{
+    use HasFactory;
+}
