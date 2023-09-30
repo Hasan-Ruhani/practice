396c06a53f6a633127541236a58238f7a424da85
H A S A N <hasan25042019@gmail.com>
2023-09-30 21:55:23 +0600

    Create ProductController.php
    fgf

---
+++ apple_p/app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\ResponseHelper;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function ListProductByCategory(Request $request): JsonResponse {
+        $data = Product::where('category_id', $request -> id) -> with('category', 'brand') -> get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+}
