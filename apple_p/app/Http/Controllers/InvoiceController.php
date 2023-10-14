0a01e9550bc922f08f9fd01419ddb12e23bfaa63
H A S A N <hasan25042019@gmail.com>
2023-10-14 21:35:53 +0600

    Update InvoiceController.php
    fg

--- apple_p/app/Http/Controllers/InvoiceController.php
+++ apple_p/app/Http/Controllers/InvoiceController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\ResponseHelper;
+use App\Helper\SSLCommerz;
 use App\Models\CustomerProfile;
+use App\Models\Invoice;
+use App\Models\InvoiceProduct;
 use App\Models\ProductCart;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceController extends Controller
 {
@@ -28,12 +33,71 @@ class InvoiceController extends Controller
             foreach($cartList as $cartItem){
                 $total = $total + $cartItem -> price;
             }
-        }
 
-        DB::commit();
+            $vat = ($total * 3) / 100;
+            $payable = $total + $vat;
+
+
+            $invoice = Invoice::create([
+                'total'=>$total,
+                'vat'=>$vat,
+                'payable'=>$payable,
+                'cus_details'=>$cus_details,
+                'ship_details'=>$ship_details,
+                'tran_id'=>$tran_id,
+                'delivery_status'=>$delivery_status,
+                'payment_status'=>$payment_status,
+                'user_id'=>$user_id
+            ]);
+
+            $invoiceID = $invoice -> id;
+
+            foreach($cartList as $EachProduct){
+                InvoiceProduct::create([
+                    'invoice_id' => $invoiceID,
+                    'product_id' => $EachProduct['product_id'],
+                    'user_id' => $user_id,
+                    'qty' => $EachProduct['qty'],
+                    'sale_price' => $EachProduct['price'],
+                ]);
+            }
+            $paymentMethod = SSLCommerz::InitiatePayment($Profile,$payable,$tran_id,$user_email);
+            
+            DB::commit();
+            return ResponseHelper::Out('success',array(['paymentMethod'=>$paymentMethod,'payable'=>$payable,'vat'=>$vat,'total'=>$total]),200);
+        }
 
         catch(Exception $e){
-
+            DB::rollBack();
+            return ResponseHelper::Out('fail', $e, 200);
         }
+    }
+
+    public function InvoiceList(Request $request) {
+        $user_id = $request -> header('id');
+        return Invoice::where('user_id', $user_id) -> get();
+    }
+
+    public function InvoiceProductList(Request $request) {
+        $user_id = $request -> header('id');
+        $invoice_id = $request -> invoice_id;
+        return InvoiceProduct::where(['user_id' => $user_id, 'invoice_id' => $invoice_id]) -> with('product') -> get();
+    }
+
+    function PaymentSuccess(Request $request){
+        return SSLCommerz::InitiateSuccess($request->query('tran_id'));
+    }
+
+
+    function PaymentCancel(Request $request){
+        return SSLCommerz::InitiateCancel($request->query('tran_id'));
+    }
+
+    function PaymentFail(Request $request){
+        return SSLCommerz::InitiateFail($request->query('tran_id'));
+    }
+
+    function PaymentIPN(Request $request){
+        return SSLCommerz::InitiateIPN($request->input('tran_id'),$request->input('status'),$request->input('val_id'));
     }
 }
