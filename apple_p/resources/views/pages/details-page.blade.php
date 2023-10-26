300cc03e7e9eb36cb2f66b998c77779215f8749b
H A S A N <hasan25042019@gmail.com>
2023-10-26 20:44:23 +0600

    Update details-page.blade.php
    dfd

--- apple_p/resources/views/pages/details-page.blade.php
+++ apple_p/resources/views/pages/details-page.blade.php
@@ -9,7 +9,7 @@
         (async () => {
             await productDetails();
             await productReview();
-            $(".preloader").delay(90).fadeOut(100).addClass('loaded');
+            // $(".preloader").delay(90).fadeOut(100).addClass('loaded');
         })()
     </script>
 @endsection
